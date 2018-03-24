8b6d1f3f8ccb1ed8effbba896f5338e5608c99de
Jan Horacek <me@apophis.cz>
2018-03-24 20:59:07 +0100

    Fix deploy script.

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,7 +4,7 @@ echo -e "Content-type: text/html\n"
 
 OUTPUT+=$(git fetch origin 2>&1)$'\n\n'
 if [ $? -eq 0 ]; then
-	OUTPUT+=$(git reset --hard origin/kscuk-web-restyle 2>&1)$'\n\n'
+	OUTPUT+=$(git reset --hard origin/master 2>&1)$'\n\n'
 fi
 
 if [ $? -eq 0 ]; then
