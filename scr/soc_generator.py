ca8751c35844cf93537b7883553536a4da1382ca
Jan Horacek <me@apophis.cz>
2020-06-23 16:33:46 +0200

    SOC: do not show closed topics.

--- scr/soc_generator.py
+++ scr/soc_generator.py
@@ -150,6 +150,7 @@ if __name__ == '__main__':
     path_garant = os.path.join(template_dir, TEMPLATE_GARANT)
 
     topics = parser.parse_topic_csv(topics)
+    topics = filter(lambda topic: topic.state != 'ukončeno', topics)
     garants = parser.parse_garant_csv(garants)
 
     with open(path_index, 'r', encoding='utf-8') as index,\
